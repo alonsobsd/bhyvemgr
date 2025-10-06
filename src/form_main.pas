312b98b4e1857d41f2dac30cf88a70b6c7bc426e
Alonso Cárdenas <acardenas@bsd-peru.org>
2025-10-06 11:53:19 -0700

    Fix issues on aarch64
    - Fix typo on DebugLn

--- src/form_main.pas
+++ src/form_main.pas
@@ -219,7 +219,7 @@ begin
   DebugLogger.CloseLogFileBetweenWrites:= true;
   DebugLogger.LogName:= GetUserDir + '.config/bhyvemgr/bhyvemgr.log';
 
-  DebugLn('['+FormatDateTime('DD-MM-YYYY HH:NN:SS', Now)+'] : '=debugln_bhyve_started);
+  DebugLn('['+FormatDateTime('DD-MM-YYYY HH:NN:SS', Now)+'] : '+debugln_bhyve_started);
 
   FormSettings:= TFormSettings.Create(FormBhyveManager);
   FormAbout:= TFormAbout.Create(FormBhyveManager);
@@ -4009,7 +4009,7 @@ begin
   if (Assigned(DeviceSettingsTreeView.Items.FindTopLvlNode('Display')))
      and (DeviceSettingsTreeView.Items.FindTopLvlNode('Display').Count = 1) then
   begin
-    DisplayNode:=DeviceSettingsTreeView.Items.TopLvlItems[2].Items[0];
+    DisplayNode:=DeviceSettingsTreeView.Items.FindTopLvlNode('Display').Items[0];
     DisplayDevice:=TDisplayDeviceClass(DisplayNode.Data);
     VncConnect(DisplayDevice.tcp, TVirtualMachineClass(VirtualMachinesTreeView.Selected.Data).name);
   end;
@@ -4163,7 +4163,8 @@ begin
       SpeedButtonRemoveVm.Enabled:=False;
       SpeedButtonReloadVmConfig.Enabled:=False;
 
-      if DeviceSettingsTreeView.Items.TopLvlItems[2].Count = 1 then
+      if (Assigned(DeviceSettingsTreeView.Items.FindTopLvlNode('Display')))
+            and (DeviceSettingsTreeView.Items.FindTopLvlNode('Display').Count = 1) then
         SpeedButtonVncVm.Enabled:=True
       else
         SpeedButtonVncVm.Enabled:=False;
